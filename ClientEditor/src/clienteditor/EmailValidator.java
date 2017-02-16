f2534ac3bdb73069cead10e992751056a67926ed
Bernat Orellana <borellan@xtec.cat>
2017-02-16 15:30:55 +0100

    canvi bernat agosarat

--- ClientEditor/src/clienteditor/EmailValidator.java
+++ ClientEditor/src/clienteditor/EmailValidator.java
@@ -39,7 +39,7 @@ import org.jdesktop.beansbinding.Validator;
 public class EmailValidator extends Validator<String> {
 
     public Validator.Result validate(String arg) {
-        if ((arg.length() < 4) || !arg.contains("@") || !arg.contains(".")) {
+        if ((arg.length() < 4) || !arg.contains("?") || !arg.contains(".")) {
             return new Result(null, "Please enter a valid email");
         }
         return null;    
