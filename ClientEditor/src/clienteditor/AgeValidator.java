99ff1fe8eb093bac1d4814afa25a6df15f1428d0
Bernat Orellana <borellan@xtec.cat>
2017-02-16 15:35:28 +0100

    canvi edat (Bernat)

--- ClientEditor/src/clienteditor/AgeValidator.java
+++ ClientEditor/src/clienteditor/AgeValidator.java
@@ -39,7 +39,7 @@ import org.jdesktop.beansbinding.Validator;
 public class AgeValidator extends Validator<Integer> {
 
     public Validator.Result validate(Integer arg) {        
-        if ((arg < 1) || (arg > 199)) {
+        if ((arg < 1) || (arg > 150)) {
             return new Result(null, "Age range is 1-199");
         }
         
